3789c3665dad45d7694fd0587d0f2a67781cb26c
Walison Leiria <wleiria@hotmail.com>
2022-03-03 00:51:34 -0300

    Aula 634 Estrutura inicial do template

---
+++ WebModerno/BaseConhecimento/frontend/src/components/template/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <span>Copyright <strong>Walison Leiria</strong> &copy; 2022</span>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+<style>
+  .footer {
+    grid-area: footer;
+    background-color: #ddd;
+    color: #333;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 10px;
+  }
+</style>
