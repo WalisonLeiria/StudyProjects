f3003bfb94d3603d121ca9fe43576cbbffabe9b0
Walison Leiria <wleiria@hotmail.com>
2022-03-04 01:55:07 -0300

    Aula 638 Componente Titulo da pagina

--- WebModerno/BaseConhecimento/frontend/src/App.vue
+++ WebModerno/BaseConhecimento/frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <Header />
+  <div id="app" :class="{'hide-menu': !isMenuVisible}">
+    <Header title="Cod3r - Base de Conhecimento" :hideToggle="false" :hideUserDropdown="false" />
     <Menu />
     <Content />
     <Footer />
@@ -13,6 +13,8 @@ import Footer from "./components/template/Footer.vue";
 import Header from "./components/template/Header.vue";
 import Menu from "./components/template/Menu.vue";
 
+import { mapState } from "vuex";
+
 export default {
   name: 'App',
   components: {
@@ -20,7 +22,8 @@ export default {
     Footer,
     Header,
     Menu
-  }
+  },
+  computed: mapState(["isMenuVisible"])
 }
 </script>
 
@@ -42,5 +45,9 @@ export default {
     grid-template-rows: 60px 1fr 40px;
     grid-template-columns: 300px 1fr;
     grid-template-areas: "header header" "menu content" "menu footer";
+  }
+
+  #app.hide-menu {
+    grid-template-areas: "header header" "content content" "footer footer";
   }
 </style>
